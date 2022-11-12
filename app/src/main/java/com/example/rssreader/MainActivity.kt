66dc51f0586b534adb72831eb84e187828398908
jehuipark <pjh2359@gmail.com>
2022-11-12 16:10:31 +0900

    Remove textViews on MainActivity
    * Remove newsCount textView
    * Remove warnings textView

--- app/src/main/java/com/example/rssreader/MainActivity.kt
+++ app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -18,26 +18,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        loadNews()
-    }
-
-    private fun loadNews() = GlobalScope.launch(dispatcher) {
-        val headlineDeferredList = feedService.fetchAllArticlesAsync(dispatcher)
-        headlineDeferredList.joinAll()
-        val headlines = headlineDeferredList.filter { !it.isCancelled }
-            .flatMap { it.await() }
-        val failed = headlineDeferredList.filter { it.isCancelled }.size
-
-        val newsCount = findViewById<TextView>(R.id.newsCount)
-        val warnings = findViewById<TextView>(R.id.warnings)
-        val obtained = feedService.feedProviderCount - failed
-        launch(Dispatchers.Main) {
-            newsCount.text = "Found ${headlines.size} News " +
-                    "in $obtained feeds"
-            if (failed > 0) {
-                warnings.text = "Failed to fetch $failed feeds"
-            }
-        }
     }
 }
