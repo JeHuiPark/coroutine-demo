6edeac63e0c3020bc95e1f8c2c43ca1774ebccac
jehuipark <pjh2359@gmail.com>
2022-11-12 16:39:17 +0900

    Add recycler view

--- app/src/main/java/com/example/rssreader/MainActivity.kt
+++ app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.rssreader
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rssreader.feed.FeedService
 import com.example.rssreader.feed.FeedServiceFactory
+import com.example.rssreader.feed.adapter.ArticleAdapter
 import kotlinx.coroutines.*
 
 @SuppressLint("SetTextI18n")
@@ -15,8 +19,31 @@ class MainActivity : AppCompatActivity() {
     private val dispatcher: CoroutineDispatcher = newFixedThreadPoolContext(2, "IO")
     private val feedService: FeedService = FeedServiceFactory.newInstance()
 
+    private lateinit var viewManager: LinearLayoutManager
+    private lateinit var viewAdapter: ArticleAdapter
+    private lateinit var articles: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = ArticleAdapter()
+        articles = findViewById<RecyclerView>(R.id.articles).apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+        loadNews()
+    }
+
+    private fun loadNews() =  GlobalScope.launch(dispatcher) {
+        val articleDeferredList = feedService.fetchAllArticlesAsync(dispatcher)
+        articleDeferredList.joinAll()
+        val articles = articleDeferredList.filter { !it.isCancelled }
+            .flatMap { it.await() }
+        launch(Dispatchers.Main) {
+            findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
+            viewAdapter.addAll(articles)
+        }
     }
 }
