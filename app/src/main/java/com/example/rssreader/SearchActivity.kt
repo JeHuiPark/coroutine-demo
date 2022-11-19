b5066961d373f88c90535d25178fff678544ab0a
jehuipark <pjh2359@gmail.com>
2022-11-19 22:28:43 +0900

    Implement SearchActivity

--- app/src/main/java/com/example/rssreader/SearchActivity.kt
+++ app/src/main/java/com/example/rssreader/SearchActivity.kt
@@ -1,12 +1,63 @@
 package com.example.rssreader
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rssreader.feed.adapter.ArticleAdapter
+import com.example.rssreader.feed.search.Searcher
+import kotlinx.coroutines.*
 
+@OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var viewManager: LinearLayoutManager
+    private lateinit var articleAdapter: ArticleAdapter
+    private lateinit var articles: RecyclerView
+    private val searcher: Searcher = Searcher()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        viewManager = LinearLayoutManager(this)
+        articleAdapter = ArticleAdapter()
+        articles = findViewById<RecyclerView>(R.id.articles).apply {
+            layoutManager = viewManager
+            adapter = articleAdapter
+        }
+
+        findViewById<Button>(R.id.searchButton).setOnClickListener {
+            articleAdapter.clear()
+            GlobalScope.launch {
+                showLoading()
+                search()
+                hideLoading()
+            }
+        }
+    }
+
+    private fun search() {
+        val query = findViewById<EditText>(R.id.searchText).text.toString()
+        val channel = searcher.search(query)
+        while(!channel.isClosedForReceive) {
+            val article = channel.tryReceive().getOrNull() ?: continue
+
+            GlobalScope.launch(Dispatchers.Main) {
+                articleAdapter.add(article)
+            }
+        }
+    }
+
+    private fun showLoading() = GlobalScope.launch(Dispatchers.Main) {
+        findViewById<ProgressBar>(R.id.progressBar).visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() = GlobalScope.launch(Dispatchers.Main) {
+        findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
     }
 }
